af8807bbf2ba3cdc5b4aa4cba1fd36966182e53f
tvuille <thomas.vuille@omnilog.fr>
2020-01-16 14:22:51 +0100

    agrandissement zone de jeu

--- Chromino/Controllers/GameController.cs
+++ Chromino/Controllers/GameController.cs
@@ -113,117 +113,11 @@ namespace ChrominoGame.Controllers
             return RedirectToAction("Show", "Game", new { id });
         }
 
-
-        // GET: Game
-        public async Task<IActionResult> Index()
-        {
-            return View(await Ctx.Games.ToListAsync());
-        }
-
-        // GET: Game/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var game = await Ctx.Games
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (game == null)
-            {
-                return NotFound();
-            }
-
-            return View(game);
-        }
-
         // GET: Game/Create
         public IActionResult Create()
         {
             GameDal.AddGame();
             return View();
-        }
-
-
-        // GET: Game/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var game = await Ctx.Games.FindAsync(id);
-            if (game == null)
-            {
-                return NotFound();
-            }
-            return View(game);
-        }
-
-        // POST: Game/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Guid,CreateDate")] Game game)
-        {
-            if (id != game.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    Ctx.Update(game);
-                    await Ctx.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GameExists(game.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(game);
-        }
-
-        // GET: Game/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var game = await Ctx.Games
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (game == null)
-            {
-                return NotFound();
-            }
-
-            return View(game);
-        }
-
-        // POST: Game/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var game = await Ctx.Games.FindAsync(id);
-            Ctx.Games.Remove(game);
-            await Ctx.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Show(int id)
