779a5b44989d9d9d003df49c85b0216050922611
tvuille <thomas.vuille@omnilog.fr>
2020-02-02 01:45:32 +0100

    ajout models

---
+++ Data/Models/ChrominoInGame.cs
@@ -0,0 +1,24 @@
+﻿using Data.Core;
+using Data.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table("ChrominoInGame")]
+    public class ChrominoInGame
+    {
+        public int Id { get; set; }
+        public byte ChrominoId { get; set; }
+        public int GameId { get; set; }
+        public Orientation Orientation { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+
+        public Chromino Chromino { get; set; }
+        public Game Game { get; set; }
+    }
+}
---
+++ Data/Models/ChrominoInHand.cs
@@ -0,0 +1,24 @@
+﻿using Data.Core;
+using Data.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table("ChrominoInHand")]
+    public class ChrominoInHand
+    {
+        public int Id { get; set; }
+        public byte ChrominoId { get; set; }
+        public int GameId { get; set; }
+        public int PlayerId { get; set; }
+        public byte Position { get; set; }
+
+        public Chromino Chromino { get; set; }
+        public Game Game { get; set; }
+        public Player Player { get; set; }
+    }
+}
