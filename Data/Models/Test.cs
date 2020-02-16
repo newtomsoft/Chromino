b2d7645924be4ad89f7e273355b11f8b195e2b55
newtom69 <thomas@vuille.fr>
2020-02-16 19:40:02 +0100

    define dev et prod

---
+++ Data/Models/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Data.Models
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int tata { get; set; }
+        public int toto { get; set; }
+        public bool Bot { get; set; }
+    }
+}
