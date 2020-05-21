6ba6b0d0eb7dfc8caea33e64c8cc7139e38d31fb
newtom69 <thomas@vuille.fr>
2020-05-21 20:30:36 +0200

    SRP MakeGameArea()

--- Chromino/wwwroot/js/domManipulation.js
+++ Chromino/wwwroot/js/domManipulation.js
@@ -415,3 +415,38 @@ function RefreshGameColors() {
 function WaikUpPlayer() {
     // todo
 }
+
+function MakeGameArea(squares, colors) {
+    let noneColor = colors.find(x => x.name == "None");
+    let cameleonColor = colors.find(x => x.name == "Cameleon");
+    let gameAreaDiv = "";
+    for (let j = 0; j < GameAreaLinesNumber; j++) {
+        gameAreaDiv += `<div id="Line_${j}" class="gameLineArea">`;
+        for (let i = 0; i < GameAreaColumnsNumber; i++) {
+            let index = i + j * GameAreaColumnsNumber;
+            let square = squares[index];
+            let colorId = square.color;
+            if (colorId != noneColor.id) {
+                let classOpenSides = "Square Open";
+                if (square.openRight) classOpenSides += "Right";
+                if (square.openBottom) classOpenSides += "Bottom";
+                if (square.openLeft) classOpenSides += "Left";
+                if (square.openTop) classOpenSides += "Top";
+                let classColor = colors.find(x => x.id == colorId).name;
+                let tip = colorId == cameleonColor ? "tip=Camelon" : "";
+                gameAreaDiv += `<div id="Square_${index}" ${tip} class="${classOpenSides} ${classColor}"></div>`;
+            }
+            else {
+                let classColor = "Free";
+                let classCloseEdge = "Square";
+                if (i < GameAreaColumnsNumber - 1 && squares[i + 1 + j * GameAreaColumnsNumber].color != noneColor.id) classCloseEdge += " CloseRight";
+                if (j < GameAreaLinesNumber - 1 && squares[i + (j + 1) * GameAreaColumnsNumber].color != noneColor.id) classCloseEdge += " CloseBottom";
+                if (i > 0 && squares[i - 1 + j * GameAreaColumnsNumber].color != noneColor.id) classCloseEdge += " CloseLeft";
+                if (j > 0 && squares[i + (j - 1) * GameAreaColumnsNumber].color != noneColor.id) classCloseEdge += " CloseTop";
+                gameAreaDiv += `<div id="Square_${index}" class="${classCloseEdge} ${classColor}"></div>`;
+            }
+        }
+        gameAreaDiv += '</div >';
+    }
+    $(gameAreaDiv).appendTo('#GameArea');
+}
