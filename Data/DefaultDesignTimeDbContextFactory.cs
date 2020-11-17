5078342a35415f5b9d8e7c3376ce47fd2d36624f
newtomsoft <thomas@newtomsoft.fr>
2020-11-17 23:11:00 +0100

    methode =>

--- Data/DefaultDesignTimeDbContextFactory.cs
+++ Data/DefaultDesignTimeDbContextFactory.cs
@@ -5,23 +5,6 @@ namespace Data
 {
     public class DefaultDesignTimeDbContextFactory : IDesignTimeDbContextFactory<Context>
     {
-        public Context CreateDbContext(string[] args)
-        {
-            return EntityFrameworkTools<Context>.CreateDbContext("Chromino");
-
-            //string path = Path.Combine(Directory.GetCurrentDirectory(), "..", "Chromino");
-            //string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            //if (env == null)
-            //    env = "Development";
-            //Console.WriteLine("ASPNETCORE_ENVIRONMENT is : " + env);
-            //IConfigurationBuilder builder = new ConfigurationBuilder()
-            //                   .SetBasePath(path)
-            //                   .AddJsonFile($"appsettings.{env}.json");
-            //IConfigurationRoot config = builder.Build();
-            //string connectionString = config.GetConnectionString("AdminDbContext");
-            //DbContextOptionsBuilder<Context> optionBuilder = new DbContextOptionsBuilder<Context>();
-            //optionBuilder.UseSqlServer(connectionString);
-            //return new Context(optionBuilder.Options);
-        }
+        public Context CreateDbContext(string[] args) => EntityFrameworkTools<Context>.CreateDbContext("Chromino");
     }
 }
